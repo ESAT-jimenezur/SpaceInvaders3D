6c57f833820b9b51b41ce1f5fc8460904522181f
Jose Luis Jimenez <joseluiszero@gmail.com>
2014-11-27 17:57:28 +0100

    Skybox Added

---
+++ Assets/src/gui/Crosshair.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+	void Start () {
+		Screen.showCursor = false;
+	}
+
+	void OnGUI(){
+		GUI.Box(new Rect(Screen.width/2,Screen.height/2, 10, 10), "");
+	}
+	//this should be an easy fix if you are impatient :D
+}
