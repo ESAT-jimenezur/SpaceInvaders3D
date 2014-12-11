58d1d2ba8b08031bf468b963ca3ca1016f99200f
Jose Luis Jimenez <joseluiszero@gmail.com>
2014-12-11 17:15:26 +0100

    some fixes

--- Assets/src/gui/Crosshair.cs
+++ Assets/src/gui/Crosshair.cs
@@ -10,5 +10,4 @@ public class Crosshair : MonoBehaviour {
 	void OnGUI(){
 		GUI.Box(new Rect(Screen.width/2,Screen.height/2, 10, 10), "");
 	}
-	//this should be an easy fix if you are impatient :D
 }
